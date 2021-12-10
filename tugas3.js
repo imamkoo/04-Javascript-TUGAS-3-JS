ae234f85cac732119321304456ba983454413092
Imamkoo <imammm.kooo@gmail.com>
2021-12-11 00:58:50 +0700

    Tugas 3 Javascript

---
+++ tugas3.js
@@ -0,0 +1,9 @@
+// Function dengan nilai balik (return)
+function ubahnilai(x) {
+    // Menampilkan nilai sebelum
+    console.log(x)
+    // ubah nilai dari variabel dan dikembalikan (return)
+    return x + 123
+}
+
+console.log(ubahnilai(321))
